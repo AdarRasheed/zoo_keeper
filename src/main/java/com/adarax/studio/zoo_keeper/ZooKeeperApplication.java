bc1fdcf7cf85a8bd068b30789c9e95f826907f68
DESKTOP-F4SQQJ1\adar <adar.rasheed@gmail.com>
2022-09-29 18:28:49 +0500

    initial commit with basic project setup

---
+++ src/main/java/com/adarax/studio/zoo_keeper/ZooKeeperApplication.java
@@ -0,0 +1,13 @@
+package com.adarax.studio.zoo_keeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooKeeperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adarax/studio/zoo_keeper/ZooKeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adarax.studio.zoo_keeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooKeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
